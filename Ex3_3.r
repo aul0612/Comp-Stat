898baacb2e16b7eac1f3d6c183eb25a74bd2c81f
aul0612 <aul0612@gmail.com>
2024-06-12 11:56:02 +0200

    Parallel Bootstrap-t

--- Ex3_3.r
+++ Ex3_3.r
@@ -16,7 +16,7 @@ for(i in 1:n){
     CI[i,1:2] <- c(mu_est - quantile * sd_est/sqrt(n), mu_est + quantile * sd_est/sqrt(n))
     CI[i, 3] <- CI[i,1]<= mu & CI[i,2]>=mu
 }
-coverage_prob <- sum(CI[,3]/n)
+coverage_prob <- sum(CI[,3])/n
 coverage_prob
 
 
@@ -29,13 +29,14 @@ for(i in 1:n){
     CI[i,1:2] <- c(mu_est - quantile * sd_est/sqrt(n), mu_est + quantile * sd_est/sqrt(n))
     CI[i, 3] <- CI[i,1]<= mu & CI[i,2]>=mu
 }
-coverage_prob <- sum(CI[,3]/n)
+coverage_prob <- sum(CI[,3])/n
 coverage_prob
 
 ### b
 # Part 2 with Standard Bootstrap
 n <- 20
 df <- 1
+mu <- df
 M <- 100000
 CI <- matrix(numeric(3*n), nrow = n, ncol = 3)
 alpha <- 0.05
@@ -57,17 +58,18 @@ for(i in 1:n){
     CI[i, 3] <- CI[i,1]<= mu & CI[i,2]>=mu
 }
 
-coverage_prob <- sum(CI[,3]/n)
+coverage_prob <- sum(CI[,3])/n
 coverage_prob
 
 
 ###c
 # Part 2 with Bootstrap-t
 # using sample variance as an unbiased estimator of asymptotic variance
-
+start_time <- Sys.time()
 n <- 20
 df <- 1
-M <- 10000000
+mu <- df
+M <- 100000
 CI <- matrix(numeric(3*n), nrow = n, ncol = 3)
 alpha <- 0.05
 bootstrap_t_func <- function(sample, sample_est, m = M){
@@ -90,8 +92,50 @@ for(i in 1:n){
     CI[i,1:2] <- c(mu_est - quants[1]*sd_est/sqrt(n), mu_est - quants[2]*sd_est/sqrt(n))
     CI[i, 3] <- CI[i,1]<= mu & CI[i,2]>=mu
 }
-
-coverage_prob <- sum(CI[,3]/n)
+end_time <- Sys.time()
+print(end_time-start_time)
+coverage_prob <- sum(CI[,3])/n
 coverage_prob
 # with M = 10,000 and M = 100,000 it is still relatively inaccurate,
 # error or are larger M needed?
+
+
+## Parallelized version:
+
+library(parallel)
+start_time <- Sys.time()
+n <- 20
+df <- 1
+M <- 100000
+CI <- matrix(numeric(3*n), nrow = n, ncol = 3)
+alpha <- 0.05
+num_cores <- detectCores() - 1  # Use one less core than available to avoid freezing the system
+
+bootstrap_t_func <- function(sample, sample_est, n, m = M){
+    cl <- makeCluster(num_cores)
+    clusterExport(cl, c("sample", "sample_est", "n"), envir = environment())
+    bootstrap_t_est <- parSapply(cl, 1:m, function(i) {
+        bootstrap_sample <- sample(sample, n, replace = TRUE)
+        bootstrap_est <- mean(bootstrap_sample)
+        bootstrap_sd_est <- sd(bootstrap_sample)
+        sqrt(n) * (bootstrap_est - sample_est) / bootstrap_sd_est
+    })
+    stopCluster(cl)
+    return(bootstrap_t_est)
+}
+
+for(i in 1:n){
+    sample <- rchisq(n, df)
+    mu_est <- mean(sample)
+    sd_est <- sd(sample)
+    bootstrap_est <- bootstrap_t_func(sample, mu_est, n)
+    quants <- quantile(bootstrap_est, c(1-alpha/2, alpha/2))
+    CI[i, 1:2] <- c(mu_est - quants[1] * sd_est / sqrt(n), mu_est - quants[2] * sd_est / sqrt(n))
+    CI[i, 3] <- (CI[i, 1] <= df) & (CI[i, 2] >= df)
+    print(i)
+}
+end_time <- Sys.time()
+print(end_time-start_time)
+
+coverage_prob <- sum(CI[, 3]) / n
+coverage_prob
